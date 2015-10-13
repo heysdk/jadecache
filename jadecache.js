7f1071979833b746712d88515d5ebf0d762dd893
zhs007 <sssxueren@gmail.com>
2015-10-13 18:39:57 +0800

    ups

--- jadecache.js
+++ jadecache.js
@@ -18,4 +18,16 @@ function render2str(filename, locals) {
     return fn(locals);
 }
 
+function rendercache2str(jadename, jadecache, locals) {
+    if (lstCache.hasOwnProperty(jadename)) {
+        return lstCache[jadename](locals);
+    }
+
+    var fn = jade.compile(jadecache, {filename: jadename});
+    lstCache[jadename] = fn;
+
+    return fn(locals);
+}
+
 exports.render2str = render2str;
+exports.rendercache2str = rendercache2str;
